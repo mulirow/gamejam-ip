092263ce76c9b8764592bcd7198243b3c644bdb7
Mulirow <mbn2@cin.ufpe.br>
2020-11-26 16:18:12 -0300

    .

---
+++ bin/backgrounds/Terrenos.tsx
@@ -0,0 +1,88 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="Tiles" tilewidth="32" tileheight="32" tilecount="28" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="32" height="32" source="agua.png"/>
+ </tile>
+ <tile id="1">
+  <image width="32" height="32" source="asfalto.png"/>
+ </tile>
+ <tile id="2">
+  <image width="32" height="32" source="grama.png"/>
+ </tile>
+ <tile id="3">
+  <image width="32" height="32" source="mandioca1.png"/>
+ </tile>
+ <tile id="4">
+  <image width="32" height="32" source="mandioca2.png"/>
+ </tile>
+ <tile id="5">
+  <image width="32" height="32" source="mandioca3.png"/>
+ </tile>
+ <tile id="6">
+  <image width="32" height="32" source="mandioca4.png"/>
+ </tile>
+ <tile id="7">
+  <image width="32" height="32" source="mandioca5.png"/>
+ </tile>
+ <tile id="8">
+  <image width="32" height="32" source="mandioca6.png"/>
+ </tile>
+ <tile id="9">
+  <image width="32" height="32" source="terra.png"/>
+ </tile>
+ <tile id="10">
+  <image width="32" height="32" source="tijoloBaixo.png"/>
+ </tile>
+ <tile id="11">
+  <image width="32" height="32" source="tijoloCima.png"/>
+ </tile>
+ <tile id="12">
+  <image width="32" height="32" source="tijoloDireita.png"/>
+ </tile>
+ <tile id="13">
+  <image width="32" height="32" source="tijoloEsquerda.png"/>
+ </tile>
+ <tile id="14">
+  <image width="32" height="32" source="tijoloH.png"/>
+ </tile>
+ <tile id="15">
+  <image width="32" height="32" source="tijoloQBaixo.png"/>
+ </tile>
+ <tile id="16">
+  <image width="32" height="32" source="tijoloQCima.png"/>
+ </tile>
+ <tile id="17">
+  <image width="32" height="32" source="tijoloQDireita.png"/>
+ </tile>
+ <tile id="18">
+  <image width="32" height="32" source="tijoloQEsquerda.png"/>
+ </tile>
+ <tile id="19">
+  <image width="32" height="32" source="tijoloQH.png"/>
+ </tile>
+ <tile id="20">
+  <image width="32" height="32" source="tijoloQV.png"/>
+ </tile>
+ <tile id="21">
+  <image width="32" height="32" source="tijoloV.png"/>
+ </tile>
+ <tile id="22">
+  <image width="32" height="32" source="interNE.png"/>
+ </tile>
+ <tile id="23">
+  <image width="32" height="32" source="interNO.png"/>
+ </tile>
+ <tile id="24">
+  <image width="32" height="32" source="interSE.png"/>
+ </tile>
+ <tile id="25">
+  <image width="32" height="32" source="interSO.png"/>
+ </tile>
+ <tile id="26">
+  <image width="32" height="32" source="areia.png"/>
+ </tile>
+ <tile id="27">
+  <image width="32" height="32" source="copacabana.png"/>
+ </tile>
+</tileset>
---
+++ bin/backgrounds/Trees.tsx
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="Trees" tilewidth="64" tileheight="61" tilecount="8" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="32" height="32" source="../entities/Trees/stump.png"/>
+ </tile>
+ <tile id="1">
+  <image width="39" height="56" source="../entities/Trees/tree1.png"/>
+ </tile>
+ <tile id="2">
+  <image width="52" height="61" source="../entities/Trees/tree2.png"/>
+ </tile>
+ <tile id="3">
+  <image width="64" height="60" source="../entities/Trees/tree3.png"/>
+ </tile>
+ <tile id="4">
+  <image width="32" height="32" source="../entities/Trees/wheat1.png"/>
+ </tile>
+ <tile id="5">
+  <image width="32" height="42" source="../entities/Trees/wheat2.png"/>
+ </tile>
+ <tile id="6">
+  <image width="32" height="42" source="../entities/Trees/wheat3.png"/>
+ </tile>
+ <tile id="7">
+  <image width="32" height="42" source="../entities/Trees/wheat4.png"/>
+ </tile>
+</tileset>
